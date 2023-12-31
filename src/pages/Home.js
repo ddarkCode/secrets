d9a392fb71e4836ac079d3d4055f2753ac3d7b45
darkcode <royal.ugoh@gmail.com>
2023-12-31 13:04:27 +0100

    Update favicon and Home page style sheet

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,9 +7,6 @@ import '../css/Home.css';
 function Home() {
   return (
     <div className="homepage-container">
-      {/* <i className="fas fa-key fa-6x"></i>
-        <h1 className="display-3">Secrets</h1>
-        <p className="lead">Don't keep your secrets, share them anonymously!</p> */}
       <div className="lc">
         <div className="logo-container">
           <img src="/logo.png" alt="logo" />
